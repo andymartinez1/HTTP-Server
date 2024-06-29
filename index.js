79c7e4a5a77b46ae7c626bf118d2de559305f75c
Andrew Martinez <andy72391@gmail.com>
2024-06-28 22:08:49 -0400

    Beginning to add paths

--- index.js
+++ index.js
@@ -2,11 +2,17 @@ const http = require("http");
 
 const PORT = 3000;
 
-const server = http.createServer((req, res) => {
-  res.writeHead(200, {
-    "Content-Type": "application/JSON",
-  });
-  res.end(JSON.stringify({ id: 1, name: "Hello" }));
+const server = http.createServer();
+
+server.on("request", (req, res) => {
+  if (req.url === "/turtles") {
+    res.writeHead(200, {
+      "Content-Type": "application/JSON",
+    });
+    res.end(JSON.stringify({ id: 1, name: "Box Turtle" }));
+  }
 });
 
-server.listen(PORT);
+server.listen(PORT, () => {
+  console.log(`Listening on port: ${PORT}`);
+});
